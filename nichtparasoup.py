7764ea081cee716ae19bd4edfdb1cfcde2c885f4
Florian Baumann <flo@noqqe.de>
2014-04-25 16:58:23 +0200

    faster image-grep with propper filtering #3

--- nichtparasoup.py
+++ nichtparasoup.py
@@ -18,7 +18,7 @@ from werkzeug.serving import run_simple
 nps_port = 5000
 nps_bindip = "0.0.0.0"
 soupiobase = "http://soup.io/"
-soupiourl = "http://soup.io/everyone"
+soupiourl = "http://soup.io/everyone?type=image"
 max_cache_imgs = 50
 logfile = "nichtparasoup.log"
 user_agent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3'
