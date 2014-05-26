f213a9c31009ee7a2165f4d1aaa890acfed013ba
Guenther Schmitz <gpunktschmitz@aol.com>
2014-05-26 14:54:52 +0200

    added burger/repositioned slider

--- templates.py
+++ templates.py
@@ -15,9 +15,11 @@ if(i){d.add(this.responseText)}if(!f.halt){h.setTimeout(d.fetch,a.interval)}}};
 e.init=function(i){f.target=document.getElementById(i);d.fetch()}})(iw={},this);</script><style type="text/css">
 html{direction:ltr;overflow:scroll;overflow-x:hidden}
 html,body{color:#ccc;background-color:black;margin:0;padding:0;height:100%}
-#header{z-index:99999;display:block;position:fixed;top:0;left:0;right:0;height:4ex;margin:0 0 .5ex;padding:0 1em;line-height:4ex;border-bottom:.5ex solid #111;background-color:#111;background-color:rgba(23,23,23,0.9);text-align:center}
+#header{z-index:99999;display:block;position:fixed;top:-42px;left:0;right:0;height:7ex;line-height:4ex;background-color:#111;background-color:rgba(23,23,23,0.9);text-align:center;border-bottom:1px solid #111;transition:all 0.6s ease-in-out 0s}
+#header:hover{top:0;border-bottom:1px solid #808080}
+input[type="range"]{position:relative;top:5px}
 #toggle{margin:0 5em;cursor:pointer;display:inline-block;width:5em}#toggle:hover{text-decoration:underline}
-#wall{margin-top:5ex;display:block;height:100%}#wallbreaker{clear:both;display:none}
+#wall{margin-top:2ex;display:block;height:100%}#wallbreaker{clear:both;display:none}
 #wall img{margin:1ex 1ex;padding:0;border:1px solid #999;display:inline;display:inline-block;float:left;max-height:80%}
 #wall img.hidden{display:none}
 #wall img.scaleIn{animation:scaleIn linear .5s;-webkit-animation:scaleIn linear .5s;-moz-animation:scaleIn linear .5s;-o-animation:scaleIn linear .5s;-ms-animation:scaleIn linear .5s;transform-origin:top left;-webkit-transform-origin:top left;-moz-transform-origin:top left;-o-transform-origin:top left;-ms-transform-origin:top left}
@@ -28,7 +30,6 @@ html,body{color:#ccc;background-color:black;margin:0;padding:0;height:100%}
 @-ms-keyframes scaleIn{from{-ms-transform:scale(0)}to{-ms-transform:scale(1)}}</style></head>
 <body onload="document.getElementById('interval').onchange(); iw.init('wall');"><div id="header">update interval: 1
 <input id="interval" max="9" min="1" onchange="iw.setInterval(this.value)" step="1" type="range" value="5"/>9
-<a id="toggle" onclick="this.innerHTML = ( iw.toggle() ? 'halt' : 'continue' );">halt</a></div><div id="wall">
+<a id="toggle" onclick="this.innerHTML = ( iw.toggle() ? 'halt' : 'continue' );">halt</a><br/>&#9776;</div><div id="wall">
 <span id="wallbreaker"></span></div><noscript>!!! enable JavaScript !!!</noscript></body></html>
 """
-
