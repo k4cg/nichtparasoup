f38ef583d7488128b547a4fdc4f01d893e9eb9dc
Jan Kowalleck <jan.kowalleck@gmail.com>
2015-01-05 01:16:35 +0100

    fix against broken builder

--- templates_raw/root/js/look_stateSwitch.js
+++ templates_raw/root/js/look_stateSwitch.js
@@ -6,7 +6,11 @@
 	var addEvent = win.helperFuncs.addEvent;
 
 	var classPrefix = 'state_';
-	var classRE = new RegExp("\\s*\\b" + classPrefix +"[01]\\b\\s*");
+
+	/* there is currently a bug in the builder that does not handle escaped chars right ... */
+	var classRE = new RegExp("\\\\s*\\\\b" + classPrefix +"[01]\\\\b\\\\s*"); // so i have to escape it doubled ..
+	var classRE = new RegExp("\\s*\\b" + classPrefix +"[01]\\b\\s*"); // this would be the correct string // @stripOnBuild
+
 
 	var setClass = function (domElem, state)
 	{
