0d4637d1ea605514672ab7e97a3af30c78452475
Jan Kowalleck <jan.kowalleck@gmail.com>
2015-01-05 01:00:47 +0100

    fixed helper functions

--- templates_raw/root/js/helper.js
+++ templates_raw/root/js/helper.js
@@ -20,14 +20,14 @@ window.helperFuncs = {
 
 };
 
-window.helperFuncs.log = function () {  // @stripOnBuild
-	window.console.log.apply(window.console, arguments);  // @stripOnBuild
-	var dbg = document.getElementById("dbg");  // @stripOnBuild
-	if ( dbg )
-	{
-		dbg.insertBefore(document.createTextNode(
-			(new Date()).toUTCString()
-			+" - "+
-			Array.prototype.join.call(arguments, ' | ') +"\n"), dbg.firstChild);
-	}
-}; // @stripOnBuild
+/* @stripOnBuild */ window.helperFuncs.log = function () {
+/* @stripOnBuild */ 	window.console.log.apply(window.console, arguments);
+/* @stripOnBuild */ 	var dbg = document.getElementById("dbg");
+/* @stripOnBuild */ 	if ( dbg )
+/* @stripOnBuild */ 	{
+/* @stripOnBuild */ 		dbg.insertBefore(document.createTextNode(
+/* @stripOnBuild */ 			(new Date()).toUTCString()
+/* @stripOnBuild */ 			+" - "+
+/* @stripOnBuild */ 			Array.prototype.join.call(arguments, ' | ') +"\n"), dbg.firstChild);
+/* @stripOnBuild */ 	}
+/* @stripOnBuild */ };
