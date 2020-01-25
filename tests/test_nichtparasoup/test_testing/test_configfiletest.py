db8677f7de2dbb323dfea2acb7e6fa093b0b0ade
Jan Kowalleck <jan.kowalleck@gmail.com>
2020-01-25 13:21:33 +0100

    prep some test

---
+++ tests/test_nichtparasoup/test_testing/test_configfiletest.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class Foo(unittest.TestCase):
+
+    def test_validate(self) -> None:
+        self.fail('NOT IMPLEMENTED')
+
+    def test_probe(self) -> None:
+        self.fail('NOT IMPLEMENTED')
