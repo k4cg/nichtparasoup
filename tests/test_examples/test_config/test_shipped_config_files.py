dbfbbaa9db746149e5b45db8c186ad1694211391
Jan Kowalleck <jan.kowalleck@gmail.com>
2020-01-25 15:47:22 +0100

    mark remotetests

--- tests/test_examples/test_config/test_shipped_config_files.py
+++ tests/test_examples/test_config/test_shipped_config_files.py
@@ -9,14 +9,14 @@ from nichtparasoup.testing.config import ConfigFileTest
 
 @ddt
 class ShippedExampleConfigFileTest(ConfigFileTest):
-
     __EXAMPLE_DIR = path_join(dirname(__file__), '..', '..', '..', 'examples', 'config')
     __EXAMPLE_FILES = glob(path_join(__EXAMPLE_DIR, '*.*'), recursive=False)
 
     def test_has_examples(self) -> None:
         self.assertGreater(len(self.__class__.__EXAMPLE_FILES), 0)
 
-    @pytest.mark.slow  # type: ignore  # probing goes over the internet and might be slow
+    @pytest.mark.online  # type: ignore  # probing goes over the internet
+    @pytest.mark.slow  # type: ignore  # probing might be slow
     @idata((basename(file),) for file in __EXAMPLE_FILES)  # type: ignore
     @unpack  # type: ignore
     def test_example(self, filename: str) -> None:
--- tests/test_nichtparasoup/test_config/test_default_config_file.py
+++ tests/test_nichtparasoup/test_config/test_default_config_file.py
@@ -6,7 +6,8 @@ from nichtparasoup.testing.config import ConfigFileTest
 
 class DefaultConfigFileTest(ConfigFileTest):
 
-    @pytest.mark.slow  # type: ignore  # probing goes over the internet and might be slow
+    @pytest.mark.online  # type: ignore  # probing goes over the internet
+    @pytest.mark.slow  # type: ignore  # probing might be slow
     def test_default(self) -> None:
         self.validate(_DEFAULTS_FILE)
         self.probe(_DEFAULTS_FILE)
