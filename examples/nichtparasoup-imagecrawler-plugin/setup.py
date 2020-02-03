8c6d9d2bca3264d6578cc45fc7cc2f85101b4100
Jan Kowalleck <jan.kowalleck@gmail.com>
2020-02-03 19:39:22 +0100

    closes #187

--- examples/nichtparasoup-imagecrawler-plugin/setup.py
+++ examples/nichtparasoup-imagecrawler-plugin/setup.py
@@ -22,7 +22,7 @@ setup(
     extras_require=dict(
         testing=[
             'mypy',
-            'pytest>=5,<5.3.4',  # 5.3.4 has a bug: https://github.com/pytest-dev/pytest/issues/6517
+            'pytest>=5.3.5',  # 5.3.4 has a bug: https://github.com/pytest-dev/pytest/issues/6517
             'coverage',
             'ddt',
         ]
