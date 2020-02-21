089a2d28a4744f12a5751f6e5b30a797bcb1dc43
Jan Kowalleck <jkowalleck@users.noreply.github.com>
2020-02-21 03:39:39 +0100

    improved code readability & maintainability related to autocompletion for yaml_file_completion

--- nichtparasoup/cli/parser.py
+++ nichtparasoup/cli/parser.py
@@ -12,7 +12,7 @@ def imagecrawler_completion(*args: Any, **kwargs: Any) -> Set[str]:
     return set(get_imagecrawlers().names())
 
 
-yaml_file_completion = FilesCompleter(('yaml', 'yml'))
+yaml_file_completion = FilesCompleter(allowednames=('yaml', 'yml'), directories=True)
 
 
 def create_parser() -> ArgumentParser:  # pragma: no cover
