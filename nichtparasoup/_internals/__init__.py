b2b6a416a8308939c7faf89d492ff5a8a52c4a1f
Jan Kowalleck <jan.kowalleck@gmail.com>
2020-03-19 22:01:19 +0100

    improv internals

--- nichtparasoup/_internals/__init__.py
+++ nichtparasoup/_internals/__init__.py
@@ -1,13 +1,12 @@
-__all__ = ['_logger', '_log', '_message', '_message_exception', '_confirm']
-
-"""
-Yes, everything is underscored.
+"""Yes, everything is underscored.
 Its internal foo that is not for public use.
 """
 
+__all__ = ["_LINEBREAK", '_LOGGER', '_log', '_logging_init', '_message', '_message_exception', '_confirm']
+
 import logging
 import sys
-from typing import Any, Optional, TextIO
+from typing import Any, List, Optional, TextIO, Union
 
 if sys.version_info >= (3, 8):
     from typing import Literal
@@ -19,38 +18,45 @@ try:
 except ImportError:
     colored = None  # type: ignore
 
-_logger = logging.getLogger('nichtparasoup')
+_LINEBREAK = '\r\n'
+
+_LOGGER = logging.getLogger('nichtparasoup')
 
 _LOG_LEVEL = Literal['debug', 'info', 'warning', 'error', 'critical', 'log', 'exception']
 
 
-def _log(level: _LOG_LEVEL, message: str, *args: Any, **kwargs: Any) -> None:
-    if not logging.root.handlers and _logger.level == logging.NOTSET:
-        _logger.setLevel(logging.INFO)
-        _logger.addHandler(logging.StreamHandler())
-    getattr(_logger, level)(message.rstrip(), *args, **kwargs)
+def _format(message: Union[str, List[str]], color: Optional[str] = None) -> str:
+    if isinstance(message, list):
+        message = _LINEBREAK.join(message)
+    if color and colored:
+        message = colored(message, color=color)
+    return message.rstrip()
 
 
 def _logging_init(level: int) -> None:  # pragma: no cover
     if not logging.root.handlers:
         logging.root.setLevel(level)
         logging.root.addHandler(logging.StreamHandler())
+    _LOGGER.setLevel(level)
 
 
-def _message(message: str, color: Optional[str] = None, file: Optional[TextIO] = None) -> None:
-    from sys import stdout
-    newline = '\r\n'
+def _log(level: _LOG_LEVEL, message: Union[str, List[str]], *args: Any, **kwargs: Any) -> None:
+    if _LOGGER.level == logging.NOTSET:
+        _logging_init(logging.INFO)
+    message = _format(message)
+    getattr(_LOGGER, level)(message, *args, **kwargs)
+
+
+def _message(message: Union[str, List[str]], color: Optional[str] = None, file: Optional[TextIO] = None) -> None:
     if not file:
-        file = stdout
-    if color and colored:
-        message = colored(message, color=color)
-    file.write('{}{}'.format(message.rstrip(), newline))
+        file = sys.stdout
+    message = _format(message, color)
+    file.write('{}{}'.format(message, _LINEBREAK))
 
 
 def _message_exception(exception: BaseException, file: Optional[TextIO] = None) -> None:
     if not file:
-        from sys import stderr
-        file = stderr
+        file = sys.stderr
     exception_name = type(exception).__name__
     if colored:
         color = 'yellow' if isinstance(exception, Warning) else 'red'
@@ -59,7 +65,7 @@ def _message_exception(exception: BaseException, file: Optional[TextIO] = None) 
 
 
 def _confirm(prompt: str, default: bool = False) -> Optional[bool]:
-    rv = {
+    return_values = {
         'y': True,
         'yes': True,
         '': default,
@@ -68,7 +74,7 @@ def _confirm(prompt: str, default: bool = False) -> Optional[bool]:
     }
     options = 'Y/n' if default else 'y/N'
     try:
-        value = input('{!s} [{}]: '.format(prompt, options)).lower().strip()
-        return rv[value]
+        value = input('{} [{}]: '.format(prompt, options)).strip().lower()
+        return return_values[value]
     except (KeyboardInterrupt, EOFError, KeyError):
         return None
