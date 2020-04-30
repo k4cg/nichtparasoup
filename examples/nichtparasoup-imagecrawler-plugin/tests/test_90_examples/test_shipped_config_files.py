056a6acf350b9b61152861800e14a4e2ba625967
Jan Kowalleck <jan.kowalleck@gmail.com>
2020-04-30 11:52:42 +0200

    tests improved

--- examples/nichtparasoup-imagecrawler-plugin/tests/test_90_examples/test_shipped_config_files.py
+++ examples/nichtparasoup-imagecrawler-plugin/tests/test_90_examples/test_shipped_config_files.py
@@ -1,25 +1,28 @@
+import unittest
 from glob import glob
-from os.path import basename, dirname, join as path_join, realpath
+from os.path import basename, dirname, join
 
 import pytest  # type: ignore
-from ddt import ddt, idata, unpack  # type: ignore
+from ddt import data, ddt  # type: ignore
 
-from nichtparasoup.testing.config import ConfigFileTest
+from nichtparasoup.config import parse_yaml_file
+from nichtparasoup.testing.config import ConfigTest
 
 
 @ddt
-class ShippedExampleConfigFileTest(ConfigFileTest):
-    __EXAMPLE_DIR = path_join(dirname(__file__), '..', '..', 'examples')
-    __EXAMPLE_FILES = glob(path_join(__EXAMPLE_DIR, '*.*'), recursive=False)
+class ShippedExampleConfigFileTest(unittest.TestCase):
+    __EXAMPLE_DIR = join(dirname(__file__), '..', '..', 'examples')
+    __EXAMPLE_FILES = [basename(f) for f in glob(join(__EXAMPLE_DIR, '*.*'), recursive=False)]
 
     def test_has_examples(self) -> None:
-        self.assertGreater(len(self.__class__.__EXAMPLE_FILES), 0)
+        self.assertGreater(len(self.__EXAMPLE_FILES), 0)
 
     @pytest.mark.online  # type: ignore  # probing goes over the internet
     @pytest.mark.slow  # type: ignore  # probing might be slow
-    @idata((basename(file),) for file in __EXAMPLE_FILES)  # type: ignore
-    @unpack  # type: ignore
+    @data(*__EXAMPLE_FILES)  # type: ignore
     def test_example(self, filename: str) -> None:
-        file = realpath(path_join(self.__class__.__EXAMPLE_DIR, filename))
-        self.validate(file)
-        self.probe(file)
+        file = join(self.__EXAMPLE_DIR, filename)
+        config = parse_yaml_file(file)
+        tester = ConfigTest()
+        tester.check_duplicates(config)
+        tester.probe(config)
--- tests/test_91_examples/test_config/test_shipped_config_files.py
+++ tests/test_91_examples/test_config/test_shipped_config_files.py
@@ -1,9 +1,9 @@
 import unittest
 from glob import glob
-from os.path import basename, dirname, join as path_join, realpath
+from os.path import basename, dirname, join
 
 import pytest  # type: ignore
-from ddt import ddt, idata, unpack  # type: ignore
+from ddt import data, ddt  # type: ignore
 
 from nichtparasoup.config import parse_yaml_file
 from nichtparasoup.testing.config import ConfigTest
@@ -11,18 +11,17 @@ from nichtparasoup.testing.config import ConfigTest
 
 @ddt
 class ShippedExampleConfigFileTest(unittest.TestCase):
-    __EXAMPLE_DIR = path_join(dirname(__file__), '..', '..', '..', 'examples', 'config')
-    __EXAMPLE_FILES = glob(path_join(__EXAMPLE_DIR, '*.*'), recursive=False)
+    __EXAMPLE_DIR = join(dirname(__file__), '..', '..', '..', 'examples', 'config')
+    __EXAMPLE_FILES = [basename(f) for f in glob(join(__EXAMPLE_DIR, '*.*'), recursive=False)]
 
     def test_has_examples(self) -> None:
         self.assertGreater(len(self.__EXAMPLE_FILES), 0)
 
     @pytest.mark.online  # type: ignore  # probing goes over the internet
     @pytest.mark.slow  # type: ignore  # probing might be slow
-    @idata((basename(file),) for file in __EXAMPLE_FILES)  # type: ignore
-    @unpack  # type: ignore
+    @data(*__EXAMPLE_FILES)  # type: ignore
     def test_example(self, filename: str) -> None:
-        file = realpath(path_join(self.__EXAMPLE_DIR, filename))
+        file = join(self.__EXAMPLE_DIR, filename)
         config = parse_yaml_file(file)
         tester = ConfigTest()
         tester.check_duplicates(config)
