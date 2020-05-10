d8ef1e69440391f793ad03787ece711f49c0d4d7
Jan Kowalleck <jan.kowalleck@gmail.com>
2020-05-10 12:36:39 +0200

    Testing: removed `ddt` depenency
    reworked tests to use `pytest.mark.parametrize` instead of `ddt`
    since `pytest` is the current test runner.
    
    closes #233

--- tests/test_10_nichtparasoup/test_imagecrawlers/test_pr0gramm.py
+++ tests/test_10_nichtparasoup/test_imagecrawlers/test_pr0gramm.py
@@ -1,131 +1,136 @@
-import unittest
 from os.path import dirname, join as path_join
-from typing import Any, Dict, Optional, Type
+from typing import Any, Dict, Optional, Sequence, Type
 from urllib.parse import parse_qs, urlsplit
 
-from ddt import data as ddt_data, ddt, idata as ddt_idata, unpack as ddt_unpack  # type: ignore
+import pytest  # type: ignore
 
 from nichtparasoup.imagecrawler import BaseImageCrawler, Image, ImageCollection
 from nichtparasoup.imagecrawlers.pr0gramm import Pr0gramm
 from nichtparasoup.testing.imagecrawler import FileFetcher, ImageCrawlerLoaderTest
 
 
-@ddt
-class Pr0grammConfigPromotedTest(unittest.TestCase):
+class TestPr0grammConfigPromoted:
 
-    @ddt_data((True,), (False,))  # type: ignore
-    @ddt_unpack  # type: ignore
-    def test__check_value(self, promoted: bool) -> None:
+    @pytest.mark.parametrize('promoted', [True, False])  # type: ignore
+    def test_check_value(self, promoted: bool) -> None:
         # arrange
         config_in = dict(promoted=promoted)
         # act
         config_out = Pr0gramm.check_config(config_in)
         # assert
-        self.assertEqual(promoted, config_out['promoted'])
+        assert promoted == config_out['promoted']
 
-    def test__check_missing_value_defaults_to_true(self) -> None:
+    def test_check_missing_value_defaults_to_true(self) -> None:
         # arrange
         config_in = dict()  # type: Dict[Any, Any]
         # act
         config_out = Pr0gramm.check_config(config_in)
         # assert
-        self.assertTrue(config_out['promoted'])
+        assert config_out['promoted'] is True
 
-    def test__check_wrong_type(self) -> None:
-        wrong_types = ['', 23, 4.2, [], (), {}, self]
-        for wrong_type in wrong_types:
-            # assert
-            with self.assertRaises(TypeError, msg=repr(wrong_type)):
-                Pr0gramm.check_config(dict(promoted=wrong_type))
+    @pytest.mark.parametrize('wrong_type', [str, int, float, list, tuple, dict, set, object])  # type: ignore
+    def test_check_wrong_type(self, wrong_type: type) -> None:
+        with pytest.raises(TypeError):
+            Pr0gramm.check_config(dict(promoted=wrong_type()))
 
 
-@ddt
-class Pr0grammConfigTagsTest(unittest.TestCase):
+class TestPr0grammConfigTags:
 
-    def test__check_missing_value(self) -> None:
+    def test_check_missing_value(self) -> None:
         # arrange
         config_in = dict()  # type: Dict[Any, Any]
         # act
         config_out = Pr0gramm.check_config(config_in)
         # assert
-        self.assertIsNone(config_out['tags'])
+        assert config_out['tags'] is None
 
-    @ddt_data(('!"test"',))  # type: ignore
-    @ddt_unpack  # type: ignore
-    def test__check_value(self, tags: str) -> None:
+    @pytest.mark.parametrize('tags', ['!"test"'])  # type: ignore
+    def test_check_value(self, tags: str) -> None:
         # arrange
         config_in = dict(tags=tags)
         # act
         config_out = Pr0gramm.check_config(config_in)
         # assert
-        self.assertEqual(tags, config_out['tags'])
+        assert tags == config_out['tags']
 
-    def test__check_wrong_tag_value(self) -> None:
-        wrong_values = ['', ' \n\t', ' !', '! \n\t']
-        for wrong_value in wrong_values:
-            # assert
-            with self.assertRaises(ValueError, msg=repr(wrong_value)):
-                Pr0gramm.check_config(dict(tags=wrong_value))
+    @pytest.mark.parametrize('wrong_value', ['', ' \n\t', ' !', '! \n\t'])  # type: ignore
+    def test_check_wrong_tag_value(self, wrong_value: str) -> None:
+        with pytest.raises(ValueError):
+            Pr0gramm.check_config(dict(tags=wrong_value))
 
-    def test__check_wrong_type(self) -> None:
-        wrong_types = [True, 23, 4.2, [], (), {}, self]
-        for wrong_type in wrong_types:
-            # assert
-            with self.assertRaises(TypeError, msg=repr(wrong_type)):
-                Pr0gramm.check_config(dict(tags=wrong_type))
+    @pytest.mark.parametrize('wrong_type', [bool, int, float, list, tuple, dict, set, object])  # type: ignore
+    def test_check_wrong_type(self, wrong_type: type) -> None:
+        with pytest.raises(TypeError):
+            Pr0gramm.check_config(dict(tags=wrong_type()))
 
 
-@ddt
-class Pr0grammUrlBuilderTest(unittest.TestCase):
+class TestPr0grammUrlBuilder:
 
     def test_base(self) -> None:
         # act
         api_uri = Pr0gramm._get_api_uri(flags=0, promoted=False)
-        (scheme, netloc, path, _, _) = urlsplit(api_uri)
+        split = urlsplit(api_uri)
         # assert
-        self.assertEqual('https', scheme)
-        self.assertEqual('pr0gramm.com', netloc)
-        self.assertEqual('/api/items/get', path)
+        assert 'https' == split.scheme
+        assert 'pr0gramm.com' == split.netloc
+        assert '/api/items/get' == split.path
 
-    @ddt_idata((i, str(i)) for i in range(1, 16, 1))  # type: ignore
-    @ddt_unpack  # type: ignore
-    def test_flags(self, flags: int, flags_qs: str) -> None:
+    @pytest.mark.parametrize(('flags', 'flags_exp'), [(i, str(i)) for i in range(1, 16, 1)])  # type: ignore
+    def test_flags(self, flags: int, flags_exp: str) -> None:
         # act
         api_uri = Pr0gramm._get_api_uri(flags=flags, promoted=False)
-        (_, _, _, query_string, _) = urlsplit(api_uri)
-        query = parse_qs(query_string)
+        query_string = urlsplit(api_uri).query
+        flags_res = parse_qs(query_string)['flags']
         # assert
-        self.assertEqual([flags_qs], query['flags'])
+        assert isinstance(flags_res, list)
+        assert flags_res == [flags_exp]
 
-    @ddt_data((True, '1'), (False, '0'))  # type: ignore
-    @ddt_unpack  # type: ignore
-    def test_promoted(self, promoted: bool, promoted_qs: str) -> None:
+    @pytest.mark.parametrize(('promoted', 'promoted_exp'), [(True, '1'), (False, '0')])  # type: ignore
+    def test_promoted(self, promoted: bool, promoted_exp: str) -> None:
         # act
         api_uri = Pr0gramm._get_api_uri(flags=0, promoted=promoted)
-        (_, _, _, query_string, _) = urlsplit(api_uri)
-        query = parse_qs(query_string)
+        query_string = urlsplit(api_uri).query
+        promoted_res = parse_qs(query_string)['promoted']
         # assert
-        self.assertEqual([promoted_qs], query['promoted'])
+        assert isinstance(promoted_res, list)
+        assert promoted_res == [promoted_exp]
 
-    @ddt_data((None, '! -"video"'), ('', '! -"video"'), ('!"test"', '!("test") -"video"'))  # type: ignore
-    @ddt_unpack  # type: ignore
-    def test_tags(self, tags: Optional[str], tags_qs: str) -> None:
+    @pytest.mark.parametrize(  # type: ignore
+        ('tags', 'tags_exp'), [
+            (None, '! -"video"'),
+            ('', '! -"video"'),
+            ('!"test"', '!("test") -"video"')
+        ])
+    def test_tags(self, tags: Optional[str], tags_exp: str) -> None:
         # act
         api_uri = Pr0gramm._get_api_uri(flags=0, promoted=False, tags=tags)
-        (_, _, _, query_string, _) = urlsplit(api_uri)
-        query = parse_qs(query_string)
+        query_string = urlsplit(api_uri).query
+        tags_res = parse_qs(query_string)['tags']
         # assert
-        self.assertEqual([tags_qs], query['tags'])
+        assert isinstance(tags_res, list)
+        assert tags_res == [tags_exp]
 
-    @ddt_data((None, None), (23, '23'))  # type: ignore
-    @ddt_unpack  # type: ignore
-    def test_older(self, older: Optional[int], older_qs: str) -> None:
+    @pytest.mark.parametrize(('older', 'older_exp'), [(None, None), (23, '23')])  # type: ignore
+    def test_older(self, older: Optional[int], older_exp: Optional[str]) -> None:
         # act
         api_uri = Pr0gramm._get_api_uri(flags=0, promoted=False, older=older)
-        (_, _, _, query_string, _) = urlsplit(api_uri)
-        query = parse_qs(query_string)
+        query_string = urlsplit(api_uri).query
+        older_res = parse_qs(query_string).get('older', [None])  # type: Sequence[Optional[str]]
         # assert
-        self.assertEqual([older_qs], query.get('older', [None]))
+        assert isinstance(older_res, list)
+        assert older_res == [older_exp]
+
+
+class TestPr0grammReset:
+
+    def test_reset_done(self) -> None:
+        # arrange
+        crawler = Pr0gramm()
+        crawler._older = 1337
+        # act
+        crawler._reset()
+        # assert
+        assert crawler._older is None
 
 
 _FILE_FETCHER = FileFetcher({  # relative to "./testdata_pr0gramm"
@@ -138,20 +143,7 @@ _FILE_FETCHER = FileFetcher({  # relative to "./testdata_pr0gramm"
 }, base_dir=path_join(dirname(__file__), 'testdata_pr0gramm'))
 
 
-class Pr0grammResetTest(unittest.TestCase):
-
-    def test_reset_done(self) -> None:
-        # arrange
-        crawler = Pr0gramm()
-        crawler._older = 1337
-        # act
-        crawler._reset()
-        # assert
-        self.assertIsNone(crawler._older)
-
-
-@ddt
-class Pr0grammCrawlTest(unittest.TestCase):
+class TestPr0grammCrawl:
 
     def test_reset_at_end(self) -> None:
         # arrange
@@ -161,7 +153,7 @@ class Pr0grammCrawlTest(unittest.TestCase):
         # act
         crawler.crawl()
         # assert
-        self.assertTrue(crawler._reset_before_next_crawl)
+        assert crawler._reset_before_next_crawl is True
 
     def test_no_reset_before_end(self) -> None:
         # arrange
@@ -171,10 +163,9 @@ class Pr0grammCrawlTest(unittest.TestCase):
         # act
         crawler.crawl()
         # assert
-        self.assertFalse(crawler._reset_before_next_crawl)
+        assert crawler._reset_before_next_crawl is False
 
-    @ddt_data((True, 503528), (False, 3652675))  # type: ignore
-    @ddt_unpack  # type: ignore
+    @pytest.mark.parametrize(('promoted', 'expected_cursor'), [(True, 503528), (False, 3652675)])  # type: ignore
     def test_crawl_cursor(self, promoted: bool, expected_cursor: Optional[int]) -> None:
         # arrange
         crawler = Pr0gramm(flags=1, promoted=promoted, tags='!s:1000')
@@ -183,7 +174,7 @@ class Pr0grammCrawlTest(unittest.TestCase):
         # act
         crawler._crawl()
         # assert
-        self.assertEqual(crawler._older, expected_cursor, msg='promoted={!r}'.format(promoted))
+        assert crawler._older == expected_cursor
 
     def test_crawl_images(self) -> None:
         # arrange
@@ -201,26 +192,26 @@ class Pr0grammCrawlTest(unittest.TestCase):
         # act
         images = crawler._crawl()
         # assert
-        self.assertSetEqual(images, expected_images)
+        assert images == expected_images
         for expected_image in expected_images:
             for image in images:
                 if image == expected_image:
                     # sources are irrelevant for equality, need to be checked manually
-                    self.assertEqual(image.source, expected_image.source)
+                    assert image.source == expected_image.source
 
 
-class Pr0grammDescriptionTest(unittest.TestCase):
+class TestPr0grammDescription:
 
     def test_description_config(self) -> None:
         # act
         description = Pr0gramm.info()
         # assert
         assert isinstance(description.config, dict)
-        self.assertIn('promoted', description.config)
-        self.assertIn('tags', description.config)
+        assert 'promoted' in description.config
+        assert 'tags' in description.config
 
 
-class Pr0grammLoaderTest(ImageCrawlerLoaderTest):
+class TestPr0grammLoader(ImageCrawlerLoaderTest):
 
     @property
     def ic_name(self) -> str:
