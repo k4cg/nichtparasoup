82f7d19bcae8effcdf0432cdba524d5ca6107835
Jan Kowalleck <jan.kowalleck@gmail.com>
2020-06-17 20:40:45 +0200

    split method `nichtparasoup.testing.ImageCrawlerTest.probe()`

--- tests/test_nichtparasoup/test_10_nichtparasoup/test_testing/test_imagecrawlertest.py
+++ tests/test_nichtparasoup/test_10_nichtparasoup/test_testing/test_imagecrawlertest.py
@@ -1,52 +1,105 @@
 import unittest
+from typing import Any, Dict, Optional
 
 from nichtparasoup.core.image import ImageCollection
 from nichtparasoup.imagecrawler import BaseImageCrawler
 from nichtparasoup.imagecrawlers.echo import Echo
-from nichtparasoup.testing.imagecrawler import ImagecrawlerProbeResult, ImageCrawlerTest
+from nichtparasoup.testing.imagecrawler import ImagecrawlerProbeResult, ImagecrawlerProbeRetryCallback, ImageCrawlerTest
+
+from . import PROBE_DELAY_IN_TESTS
 
 
-class TestImagecrawlerProbeResultIsFailure:
+class TestImageCrawlerProbe:
 
-    def test_failure_no_errors(self) -> None:
+    def test_once(self) -> None:
         # arrange
-        res = ImagecrawlerProbeResult(None, [])
-        # act & assert
-        assert res.is_failure
-        assert not res.is_erroneous
+        tester = ImageCrawlerTest(Echo(image_uri='https://foo.bar'))
+        probe_once_called = []
+        probe_once_result = ImagecrawlerProbeResult(None, [])
 
-    def test_failure_with_error(self) -> None:
+        def fake_once() -> ImagecrawlerProbeResult:
+            probe_once_called.append(True)
+            return probe_once_result
+
+        tester.probe_once = fake_once  # type: ignore[assignment]
+        # act
+        result = tester.probe(retries=0)
+        # assert
+        assert [True] == probe_once_called
+        assert probe_once_result is result
+
+    def test_until_success(self) -> None:
         # arrange
-        res = ImagecrawlerProbeResult(None, [Exception()])
-        # act & assert
-        assert res.is_failure
-        assert res.is_erroneous
+        tester = ImageCrawlerTest(Echo(image_uri='https://foo.bar'))
+        probe_until_success_called = []
+        probe_until_success_result = ImagecrawlerProbeResult(None, [])
+        probe_params: Dict[str, Any] = {
+            'retries': 23,
+            'retry_delay': 1.337,
+            'retry_callback': lambda _, __: True,
+        }
 
-    def test_success_no_errors(self) -> None:
+        def fake_until_success(*,
+                               retries: int,
+                               retry_delay: float,
+                               retry_callback: Optional[ImagecrawlerProbeRetryCallback]
+                               ) -> ImagecrawlerProbeResult:
+            probe_until_success_called.append({
+                'retries': retries,
+                'retry_delay': retry_delay,
+                'retry_callback': retry_callback,
+            })
+            return probe_until_success_result
+
+        tester.probe_until_success = fake_until_success  # type: ignore[assignment]
+        # act
+        result = tester.probe(**probe_params)
+        # assert
+        assert [probe_params] == probe_until_success_called
+        assert probe_until_success_result is result
+
+
+class TestImageCrawlerProbeOnce:
+
+    def test_success(self) -> None:
         # arrange
-        res = ImagecrawlerProbeResult(ImageCollection(), [])
-        # act & assert
-        assert not res.is_failure
-        assert not res.is_erroneous
+        tester = ImageCrawlerTest(Echo(image_uri='https://foo.bar'))
+        images = ImageCollection()
+        tester.imagecrawler._crawl = lambda: images  # type: ignore[assignment]
+        # act
+        result = tester.probe_once()
+        # assert
+        assert images is result.images
+        assert [] == result.errors
 
-    def test_success_with_errors(self) -> None:
+    def test_failed(self) -> None:
         # arrange
-        res = ImagecrawlerProbeResult(ImageCollection(), [Exception()])
-        # act & assert
-        assert not res.is_failure
-        assert res.is_erroneous
+        tester = ImageCrawlerTest(Echo(image_uri='https://foo.bar'))
+        error = BaseException()
+
+        def fake_crawl() -> ImageCollection:
+            raise error
+
+        tester.imagecrawler._crawl = fake_crawl   # type: ignore[assignment]
+        # act
+        result = tester.probe_once()
+        # assert
+        assert None is result.images
+        assert [error] == result.errors
 
 
-class ImageCrawlerProbeTest(unittest.TestCase):
+class ImageCrawlerProbeUntilSuccessTest(unittest.TestCase):
+
+    # TODO write the test with faked probe_once
 
     def test_success(self) -> None:
         # arrange
         images = ImageCollection()
         imagecrawler = Echo(image_uri='https://foo.bar')
-        imagecrawler._crawl = lambda: images  # type: ignore
+        imagecrawler._crawl = lambda: images  # type: ignore[assignment]
         tester = ImageCrawlerTest(imagecrawler)
         # act
-        result = tester.probe()
+        result = tester.probe_until_success(retry_delay=PROBE_DELAY_IN_TESTS)
         # assert
         self.assertEqual(images, result.images)
         self.assertListEqual([], result.errors)
@@ -59,10 +112,10 @@ class ImageCrawlerProbeTest(unittest.TestCase):
             raise error
 
         imagecrawler = Echo(image_uri='https://foo.bar')
-        imagecrawler._crawl = crawl_fail  # type: ignore
+        imagecrawler._crawl = crawl_fail  # type: ignore[assignment]
         tester = ImageCrawlerTest(imagecrawler)
         # act
-        result = tester.probe(retries=0, retry_delay=0)
+        result = tester.probe_until_success(retries=0, retry_delay=PROBE_DELAY_IN_TESTS)
         # assert
         self.assertIsNone(result.images)
         self.assertListEqual([error], result.errors)
@@ -84,10 +137,10 @@ class ImageCrawlerProbeTest(unittest.TestCase):
             raise error
 
         imagecrawler = Echo(image_uri='https://foo.bar')
-        imagecrawler._crawl = crawl  # type: ignore
+        imagecrawler._crawl = crawl  # type: ignore[assignment]
         tester = ImageCrawlerTest(imagecrawler)
         # act
-        result = tester.probe(retries=retries, retry_delay=0)
+        result = tester.probe_until_success(retries=retries, retry_delay=PROBE_DELAY_IN_TESTS)
         # assert
         self.assertEqual(images, result.images)
         self.assertListEqual([error] * retries, result.errors)
@@ -114,10 +167,11 @@ class ImageCrawlerProbeTest(unittest.TestCase):
             raise error
 
         imagecrawler = Echo(image_uri='https://foo.bar')
-        imagecrawler._crawl = crawl  # type: ignore
+        imagecrawler._crawl = crawl  # type: ignore[assignment]
         tester = ImageCrawlerTest(imagecrawler)
         # act
-        result = tester.probe(retries=retries, retry_delay=0, retry_callback=retry_callback)
+        result = tester.probe_until_success(retries=retries, retry_delay=PROBE_DELAY_IN_TESTS,
+                                            retry_callback=retry_callback)
         # assert
         self.assertEqual(images, result.images)
         self.assertListEqual([error] * retries, result.errors)
@@ -137,10 +191,11 @@ class ImageCrawlerProbeTest(unittest.TestCase):
             raise error
 
         imagecrawler = Echo(image_uri='https://foo.bar')
-        imagecrawler._crawl = crawl_fail  # type: ignore
+        imagecrawler._crawl = crawl_fail  # type: ignore[assignment]
         tester = ImageCrawlerTest(imagecrawler)
         # act
-        result = tester.probe(retries=retries, retry_delay=0, retry_callback=retry_callback)
+        result = tester.probe_until_success(retries=retries, retry_delay=PROBE_DELAY_IN_TESTS,
+                                            retry_callback=retry_callback)
         # assert
         self.assertEqual(None, result.images)
         self.assertListEqual([error], result.errors)
