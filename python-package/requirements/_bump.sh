65303af80744433cf7e711eb6e3d3c39f35e6229
Jan Kowalleck <jan.kowalleck@gmail.com>
2020-07-29 21:32:14 +0200

    removed constraints file
    since we want to periodically test the latest versions if package
    requirements, no constraints were taken.

--- python-package/requirements/_bump.sh
+++ python-package/requirements/_bump.sh
@@ -4,8 +4,6 @@ set -e
 # dont wanna have nasty path prefixes in created files. so simply cd in here
 cd "$(dirname "$0")"
 
-# all requirements are for build/dev
-# so it is a MUST that also pip/setuptools is pinned -> use `--allow-unsafe`
 find -type f -name '*.in' \
   -exec echo 'precessing {}' \; \
-  -exec pip-compile -qUr --allow-unsafe {} \;
+  -exec pip-compile -qUr {} \;
