887b9d68f97b49fdc31844bb4a06b43996bf8445
Jan Kowalleck <jan.kowalleck@gmail.com>
2020-11-23 10:55:30 +0100

    some exampe

--- python-plugin-example/tests/unit/test_DummyImage.py
+++ python-plugin-example/tests/unit/test_DummyImage.py
@@ -1,105 +1,143 @@
+from random import randint, random
 from typing import Any
+from unittest.mock import Mock
+from urllib.parse import urlparse
 
 import pytest
 
-from nichtparasoup_imagecrawler_dummyimage import DummyImage
+from nichtparasoup_imagecrawler_dummyimage import DummyImage as Sut
 
-_DUMMYIMAGE_RIGHT_CONFIG = {'width': 800, 'height': 600}
+_DUMMYIMAGE_RIGHT_CONFIG = {'width': randint(1, 999), 'height': randint(1, 999)}
 
 
 class TestDummyImageConfigCorrect:
 
-    def test__check_config_right_value(self) -> None:
+    @staticmethod
+    def test_right_value() -> None:
         # arrange
         config_in = _DUMMYIMAGE_RIGHT_CONFIG.copy()
         # act
-        config_out = DummyImage.check_config(config_in)
+        config_out = Sut.check_config(config_in)
         # assert
-        assert config_out == config_in
+        assert config_out == _DUMMYIMAGE_RIGHT_CONFIG
+
+    @staticmethod
+    def test_unknown_ignored() -> None:
+        # arrange
+        config_in = _DUMMYIMAGE_RIGHT_CONFIG.copy()
+        config_in['unknown'] = 42
+        # act
+        config_out = Sut.check_config(config_in)
+        # assert
+        assert config_out == _DUMMYIMAGE_RIGHT_CONFIG
 
 
 class TestDummyImageConfigWidth:
 
-    def test__check_config_missing_value(self) -> None:
+    @staticmethod
+    def test_missing_value() -> None:
         # arrange
         config_in = _DUMMYIMAGE_RIGHT_CONFIG.copy()
         del config_in['width']
         # assert
         with pytest.raises(KeyError):
             # act
-            DummyImage.check_config(config_in)
+            Sut.check_config(config_in)
 
-    @pytest.mark.parametrize('wrong_type', [None, True, '', 0.001, list(), set(), dict(), tuple(), object()], ids=type)
-    def test__check_config_wrong_type(self, wrong_type: Any) -> None:
+    @staticmethod
+    @pytest.mark.parametrize('wrong_type',
+                             [None, True, '', random(), list(), set(), dict(), tuple(), object()],
+                             ids=type)
+    def test_wrong_type(wrong_type: Any) -> None:
         # arrange
         config_in = _DUMMYIMAGE_RIGHT_CONFIG.copy()
         config_in['width'] = wrong_type
         # assert
         with pytest.raises(TypeError):
             # act
-            DummyImage.check_config(config_in)
+            Sut.check_config(config_in)
 
-    @pytest.mark.parametrize('wrong_value', [0, -23], ids=['zero', 'negative'])
-    def test__check_config_wrong_value(self, wrong_value: int) -> None:
+    @staticmethod
+    @pytest.mark.parametrize('wrong_value', [0, -randint(1, 999)], ids=['zero', 'negative'])
+    def test_wrong_value(wrong_value: int) -> None:
         # arrange
         config_in = _DUMMYIMAGE_RIGHT_CONFIG.copy()
         config_in['width'] = wrong_value
         # assert
         with pytest.raises(ValueError):
             # act
-            DummyImage.check_config(config_in)
+            Sut.check_config(config_in)
 
 
 class TestDummyImageConfigHeight:
 
-    def test__check_config_missing_value(self) -> None:
+    @staticmethod
+    def test_missing_value() -> None:
         # arrange
         config_in = _DUMMYIMAGE_RIGHT_CONFIG.copy()
         del config_in['height']
         # assert
         with pytest.raises(KeyError):
             # act
-            DummyImage.check_config(config_in)
+            Sut.check_config(config_in)
 
-    @pytest.mark.parametrize('wrong_type', [None, True, '', 0.001, list(), set(), dict(), tuple(), object()], ids=type)
-    def test__check_config_wrong_type(self, wrong_type: Any) -> None:
+    @staticmethod
+    @pytest.mark.parametrize('wrong_type',
+                             [None, True, '', random(), list(), set(), dict(), tuple(), object()],
+                             ids=type)
+    def test_wrong_type(wrong_type: Any) -> None:
         # arrange
         config_in = _DUMMYIMAGE_RIGHT_CONFIG.copy()
         config_in['height'] = wrong_type
         # assert
         with pytest.raises(TypeError):
             # act
-            DummyImage.check_config(config_in)
+            Sut.check_config(config_in)
 
-    @pytest.mark.parametrize('wrong_value', [0, -42], ids=['zero', 'negative'])
-    def test__check_config_wrong_value(self, wrong_value: int) -> None:
+    @staticmethod
+    @pytest.mark.parametrize('wrong_value', [0, -randint(1, 999)], ids=['zero', 'negative'])
+    def test_wrong_value(wrong_value: int) -> None:
         # arrange
         config_in = _DUMMYIMAGE_RIGHT_CONFIG.copy()
         config_in['height'] = wrong_value
         # assert
         with pytest.raises(ValueError):
             # act
-            DummyImage.check_config(config_in)
+            Sut.check_config(config_in)
 
 
-class TestDummyImageCrawl:
-
-    def test_crawl(self) -> None:
-        # arrange
-        crawler = DummyImage(**_DUMMYIMAGE_RIGHT_CONFIG)
-        # act
-        images_crawled = crawler._crawl()
-        # assert
-        assert len(images_crawled) == crawler._BUNCH, 'crawler did not finish'
-        for image_crawled in images_crawled:
-            assert image_crawled.is_generic is True, 'this is not generic'
+def test_always_exhausted() -> None:
+    # act
+    exhausted = Sut.is_exhausted(Mock())
+    # assert
+    assert not exhausted
 
 
-class TestDummyImageDescription:
+def test_get_image_uri() -> None:
+    width = randint(1, 999)
+    height = randint(1, 999)
+    # act
+    url = Sut._get_image_uri(width=width, height=height)
+    url_parts = urlparse(url)
+    # assert
+    assert url_parts.hostname == 'dummyimage.com'
+    assert url_parts.path.startswith(f'/{width}x{height}/')
 
-    def test_description_config(self) -> None:
-        # act
-        description = DummyImage.info()
-        # assert
-        assert isinstance(description.config, dict)
-        assert _DUMMYIMAGE_RIGHT_CONFIG.keys() == description.config.keys()
+
+def test_crawl() -> None:
+    # arrange
+    crawler = Sut(**_DUMMYIMAGE_RIGHT_CONFIG)
+    # act
+    images_crawled = crawler._crawl()
+    # assert
+    assert len(images_crawled) == crawler._BUNCH, 'crawler did not finish'
+    for image_crawled in images_crawled:
+        assert image_crawled.is_generic is True, 'this is not generic'
+
+
+def test_description_config() -> None:
+    # act
+    description = Sut.info()
+    # assert
+    assert isinstance(description.config, dict)
+    assert _DUMMYIMAGE_RIGHT_CONFIG.keys() == description.config.keys()
