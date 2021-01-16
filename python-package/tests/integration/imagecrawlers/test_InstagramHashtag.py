45d88ef872ab1719c7403a56f8255167ca8a3f88
Jan Kowalleck <jan.kowalleck@gmail.com>
2021-01-16 11:24:15 +0100

    removed instagram integration tests

--- python-package/tests/integration/imagecrawlers/test_InstagramHashtag.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from nichtparasoup.imagecrawlers.instagram import InstagramHashtag
-from nichtparasoup.testing.imagecrawler import ImageCrawlerLoaderTest
-
-
-@pytest.mark.no_cover
-def test_loader() -> None:
-    ImageCrawlerLoaderTest().check('InstagramHashtag', InstagramHashtag)
--- python-package/tests/integration/imagecrawlers/test_InstagramProfile.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from nichtparasoup.imagecrawlers.instagram import InstagramProfile
-from nichtparasoup.testing.imagecrawler import ImageCrawlerLoaderTest
-
-
-@pytest.mark.no_cover
-def test_loader() -> None:
-    ImageCrawlerLoaderTest().check('InstagramProfile', InstagramProfile)
